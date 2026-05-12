d900126c3b42c06f6a206eceb2ee90d20aa36519
tharatip soongkhaw <thratip.so@up.ac.th>
2026-05-12 10:16:21 +0700

    chore: update live V2 endpoint URL

--- Team IROUP/iroup-v2-endpoint.js
+++ Team IROUP/iroup-v2-endpoint.js
@@ -1,1 +1,1 @@
-window.IROUP_V2_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxaFDS2Ifijsb4OYthoXdKjuX-skAL5R3ve4raQCSUWyzBynG369nLMmYIY6PY0yg8z/exec';
+window.IROUP_V2_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwD8_UowgPCCoiXuFDNSqvLepuw5BTnUhShPeJUISmYbzLmhkt6KGrr3Jj5rAWlQWSw8Q/exec';
